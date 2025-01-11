04ff46384573832fed7c8ec3640cdb814a14c729
Dund <dundzds@gmail.com>
2025-01-11 17:58:04 +0300

    hw 8 commit demoqa

--- tests/test_hw/test_visible_hw.py
+++ tests/test_hw/test_visible_hw.py
@@ -1,17 +1,22 @@
 import time
-from pages.accordian import Accordian
+from pages.accordion import Accordion
 
 
-def text_visible_accordian(browser):
-
-    accordian_page = Accordian(browser)
+def text_visible_accordian (browser):
+    accordian_page = Accordion(browser)
     accordian_page.visit()
+    assert accordian_page.section_1_content.visible()
+    # accordian_page.section_heading.click()
+    # time.sleep(2)
+    # assert not accordian_page.section_1_content.visible()
 
-    assert accordian_page.section_content.visible()
-    accordian_page.section_heading.click()
-    time.sleep(2)
-    assert not accordian_page.section_content.visible()
 
+# def test_visible_accordion_default(browser):
+#     accordian_page = Accordion(browser)
+#     accordian_page.visit()
+#     assert not accordian_page.section_2_content_child_1.visible()
+#     assert not accordian_page.section_2_content_child_2.visible()
+#     assert not accordian_page.section_3_content.visible()
 
 
 
